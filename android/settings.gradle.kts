c35a42a6589dcfb6082de70d40b968986ac9e941
kirasimon565 <kirasimon136@gmail.com>
2026-06-29 20:08:58 +0300

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,3 +1,27 @@
+pluginManagement {
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        val localProperties = file("local.properties")
+        if (localProperties.exists()) {
+            localProperties.inputStream().use { properties.load(it) }
+            properties.getProperty("flutter.sdk")
+        } else {
+            null
+        }
+    }
+    
+    if (flutterSdkPath != null) {
+        settings.extra.set("flutterSdkPath", flutterSdkPath)
+        includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+    }
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "9.0.0" apply false
