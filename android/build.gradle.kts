757b2b017d087b7bd8b66811d97ce458e62c8937
kirasimon565 <kirasimon136@gmail.com>
2026-06-29 19:56:46 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,14 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:9.0.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
+    }
+}
+
 allprojects {
     repositories {
         google()
